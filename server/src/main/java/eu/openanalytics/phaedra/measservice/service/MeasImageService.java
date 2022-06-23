93463c6aa0d527cde32c3256bb8d2947ec68ffde
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-06-23 18:03:40 +0200

    Fixed libloading issue, added missing license headers

--- server/src/main/java/eu/openanalytics/phaedra/measservice/service/MeasImageService.java
+++ server/src/main/java/eu/openanalytics/phaedra/measservice/service/MeasImageService.java
@@ -1,3 +1,23 @@
+/**
+ * Phaedra II
+ *
+ * Copyright (C) 2016-2022 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.phaedra.measservice.service;
 
 import java.io.IOException;
@@ -10,6 +30,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import eu.openanalytics.phaedra.imaging.jp2k.ICodestreamSource;
+import eu.openanalytics.phaedra.imaging.jp2k.openjpeg.OpenJPEGLibLoader;
 import eu.openanalytics.phaedra.imaging.jp2k.openjpeg.source.ByteArraySource;
 import eu.openanalytics.phaedra.imaging.render.ImageRenderConfig;
 import eu.openanalytics.phaedra.imaging.render.ImageRenderService;
@@ -18,6 +39,10 @@ import eu.openanalytics.phaedra.measservice.dto.MeasurementDTO;
 @Service
 public class MeasImageService {
 
+	static {
+		OpenJPEGLibLoader.load();
+	}
+	
 	@Autowired
 	private MeasService measService;
 	
