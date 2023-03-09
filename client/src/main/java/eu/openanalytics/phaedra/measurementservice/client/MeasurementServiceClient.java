761933a97fe038bf5a294e3ef4b18a0ff8acabc0
Frederick Michielssen <fmichielssen@openanalytics.eu>
2023-03-09 16:42:33 +0100

    Added API: getSubWellData

--- client/src/main/java/eu/openanalytics/phaedra/measurementservice/client/MeasurementServiceClient.java
+++ client/src/main/java/eu/openanalytics/phaedra/measurementservice/client/MeasurementServiceClient.java
@@ -24,11 +24,13 @@ import eu.openanalytics.phaedra.measservice.dto.MeasurementDTO;
 import eu.openanalytics.phaedra.measurementservice.client.exception.MeasUnresolvableException;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MeasurementServiceClient {
 
     float[] getWellData(long measId, String columnName) throws MeasUnresolvableException;
-
+    Map<Integer, float[]> getSubWellData(long measId, String columnName) throws MeasUnresolvableException;
+    
     List<MeasurementDTO> getMeasurementsByMeasIds(long ...measId);
 
 }
